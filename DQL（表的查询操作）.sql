332a78cad54f96a8503cc956994d52da8e7d1a80
YuMengMeng <whoyumengmeng@163.com>
2022-05-13 08:46:40 +0800

    transaction

--- DQL（表的查询操作）.sql
+++ DQL（表的查询操作）.sql
@@ -64,3 +64,78 @@ SELECT ename, salary + 1000 FROM emp;
 		逻辑运算符：&&（and） ||（or） not 
 			
 */
+
+/*
+	排序
+	order by子句
+	语法结构：
+	select 字段名 from 表名 [where 字段名 = 值] order by 字段名[asc/desc] 
+	
+	
+*/
+
+-- 单列排序，按照某一个字段排序
+USE db2;
+SELECT * FROM emp ORDER BY salary;
+
+SELECT * FROM emp ORDER BY salary DESC;
+
+-- 组合排序
+SELECT * FROM emp ORDER BY salary DESC, eid DESC;
+
+
+
+/*
+	聚合函数：将一列数据作为一个整体，进行纵向计算
+	常用聚合函数
+		count(字段)
+		sum(字段)
+		max(字段)
+		min(字段)
+		avg(字段)
+		
+	格式：
+	select 聚合函数名(字段) from 表名 [where 条件]
+	
+*/
+
+#1.查询员工总数
+SELECT * FROM emp;
+SELECT COUNT(eid) '员工总数' FROM emp;
+
+/*
+	分组查询：group by子句
+	select 分组字段/聚合函数 from 表名 group by 分组字段
+	
+	
+*/
+SELECT * FROM emp GROUP BY sex;
+SELECT sex FROM emp GROUP BY sex; # 按性别分组，只返回分组的第一条数据
+
+# 通过性别字段进行分组，求平均薪资
+SELECT sex, AVG(salary) FROM emp GROUP BY sex;
+
+# 查询平均薪资大于6000的部门
+-- 1.首先要分组求平均薪资
+-- 2.求出平均薪资大于6000的部门
+-- 分组之后进行条件过滤：having
+
+
+SELECT 
+	dept_name,
+	AVG(salary)
+FROM emp
+WHERE dept_name IS NOT NULL GROUP BY dept_name
+HAVING AVG(salary)>6000;
+
+/*
+	limit 限制查询数据条数
+	语法格式：
+		select 字段 from 表 limit offset,length;
+	参数说明：
+		offset：起始行数，默认0
+		length：返回数据条数
+*/
+
+#查询前5条数据
+SELECT * FROM emp LIMIT 5,5;
